efcb1c1d1136ea87a2e653c7233a8b6d4727a070
pollob debnath <pullobdebnath@gmail.com>
2025-11-28 19:23:06 +0600

    header done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Web Developer", "Competitive Programmer"],
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1000,
-    loop: true
+  strings: ["Frontend Developer", "Web Developer", "Competitive Programmer"],
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1000,
+  loop: true,
 });
