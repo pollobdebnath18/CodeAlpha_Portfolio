45f97c249c7626d2ca907db72541e01a0aa8653a
pollob debnath <pullobdebnath@gmail.com>
2025-12-01 09:19:51 +0600

    modify about section

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true,
 });
+
+const tabLinks = document.getElementsByClassName('tab-link');
+const tabContents = document.getElementsByClassName('tab-contents');
+
+function TabOpen(value){
+  for(tabLink of tabLinks){
+    tabLink.classList.remove('active-link');
+  }
+  for(tabContent of tabContents){
+    tabContent.classList.remove('active-tab');
+  }
+  event.currentTarget.classList.add('active-link');
+  document.getElementById(value).classList.add('active-tab');
+}
